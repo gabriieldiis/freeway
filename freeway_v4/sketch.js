8a8282ab11f3d5b6a0f757a561e77505ce9e43c5
Gabriel Dias <87398682+gabriieldiis@users.noreply.github.com>
2023-03-02 13:59:45 -0300

    Comit 02/03/2023
    adicionei a potuação do jogo

---
+++ freeway_v4/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 400);
+}
+
+
+function draw() {
+  background(imagemEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro()
+  movimentaAtor()
+  voltaPosicaoInicialDoCarro()
+  verificaColisao()
+  incluiPontos()
+  marcaPonto()
+}
